fc60c2198b6371892435dee7705e67aa3d42cf1c
jmj <waysoleward01@naver.com>
2026-01-13 04:51:05 +0900

    feat: 밑줄 투명도 조절

--- component/common/index.tsx
+++ component/common/index.tsx
@@ -13,12 +13,15 @@ export function EmptyRowCol<T = {}>({ children }: PropsWithChildren<T>) {
 const StyledLink = styled.a`
   color: inherit;
   text-decoration: underline;
+  text-decoration-color: rgba(0, 0, 0, 1);
+  text-decoration-thickness: 1px;
   font-weight: normal;
   transition: all 0.2s ease-in-out;
 
   &:hover {
     color: #2997fb;
     text-decoration: underline;
+    text-decoration-color: #2997fb;
     text-decoration-thickness: 2px;
   }
 `;
