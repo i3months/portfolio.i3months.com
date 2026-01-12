0f6fd561087d606c19247136b08080660fb692f8
jmj <waysoleward01@naver.com>
2026-01-12 23:40:05 +0900

    feat: 호버링 액션 추가

--- component/common/index.tsx
+++ component/common/index.tsx
@@ -1,5 +1,6 @@
 import { Row, Col } from 'reactstrap';
 import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
 
 export function EmptyRowCol<T = {}>({ children }: PropsWithChildren<T>) {
   return (
@@ -9,15 +10,22 @@ export function EmptyRowCol<T = {}>({ children }: PropsWithChildren<T>) {
   );
 }
 
+const StyledLink = styled.a`
+  color: inherit;
+  text-decoration: underline;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    color: #2997fb;
+    text-decoration: underline;
+    text-decoration-thickness: 2px;
+  }
+`;
+
 export function HrefTargetBlank({ url, text }: PropsWithChildren<{ url: string; text?: string }>) {
   return (
-    <a
-      href={url}
-      target="_blank"
-      rel="noreferrer noopener"
-      style={{ color: 'inherit', textDecoration: 'underline' }}
-    >
+    <StyledLink href={url} target="_blank" rel="noreferrer noopener">
       {text || url}
-    </a>
+    </StyledLink>
   );
 }
