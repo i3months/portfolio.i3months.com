2e5685cb6205f58bc449653910e595deacebecf6
jmj <waysoleward01@naver.com>
2026-01-13 05:49:21 +0900

    feat: eziwiki에 codacy 추가

--- payload.en/openSource.ts
+++ payload.en/openSource.ts
@@ -34,6 +34,10 @@ const openSource: IOpenSource.Payload = {
           content: 'Github Stars: ',
           postImage: 'https://img.shields.io/github/stars/i3months/eziwiki.svg?style=popout',
         },
+        {
+          content: 'Code Quality: ',
+          postImage: 'https://app.codacy.com/project/badge/Grade/52948e0fb28648bba1c3ab8f4a3919f9',
+        },
       ],
     },
   ],
